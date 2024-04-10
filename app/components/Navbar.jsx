1a83efcaacb1bcbe7b9e735e6ba6c19f5c3a7701
hayes28 <heather.hayes918@gmail.com>
2024-04-10 09:40:51 -0500

    Add new pages and components for Dashboard and Tickets
    As well as, added json data and json-server to use for project

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "./dojo-logo.png"
 
 export default function Navbar() {
     return (
         <nav>
+            <Image
+                src={Logo}
+                alt="Ninja Logo"
+                width={70}
+                quality={100}
+                placeholder="blur"
+            />
             <h1> Net Ninja HelpDesk </h1>
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,7 +1,15 @@
+import TicketsList from "./TicketsList";
+
 export default function Tickets() {
-    return (
-        <main>
-        <h2>Tickets</h2>
-        </main>
-    )
+  return (
+    <main>
+      <nav>
+        <div>
+          <h1>Tickets</h1>
+          <p><small>Currently open tickets</small></p>
+        </div>
+      </nav>
+      <TicketsList />
+    </main>
+  )
 }
