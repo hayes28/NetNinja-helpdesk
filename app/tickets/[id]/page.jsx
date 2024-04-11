53f58ef0a858d7758da33d2552b2025fbcfd4a5d
hayes28 <heather.hayes918@gmail.com>
2024-04-10 20:01:47 -0500

    Updated evn variables

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation"
 export const dynamicParams = true // default val = true
 
 export async function generateStaticParams() {
-  const res = await fetch('http://localhost:4000/tickets')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets`)
 
   const tickets = await res.json()
 
@@ -15,8 +15,8 @@ export async function generateStaticParams() {
 async function getTicket(id) {
   // imitate delay
   await new Promise((resolve) => setTimeout(resolve, 3000))
-  
-  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+
+  const res = await fetch(`${ process.env.NEXT_PUBLIC_API_URL }/tickets${id}`, {
     next: {
       revalidate: 60
     }
--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -17,7 +17,7 @@ export default function CreateForm() {
 
         const newTicket = { title, body, priority, user_email: 'mario@netninja.dev' }
 
-        const res = await fetch('http://localhost:4000/tickets', {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(newTicket)
