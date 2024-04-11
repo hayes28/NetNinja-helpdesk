79116f73028b10c0ede9512296a569b185c1bd5a
hayes28 <heather.hayes918@gmail.com>
2024-04-10 19:42:00 -0500

    Add new ticket creation functionality and update ticket page with "New Ticket" button And ran build

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import TicketsList from "./TicketsList";
 import Loading from "../loading";
+import Link from "next/link";
 
 export default function Tickets() {
   return (
@@ -10,6 +11,9 @@ export default function Tickets() {
           <h1>Tickets</h1>
           <p><small>Currently open tickets</small></p>
         </div>
+        <Link href="/tickets/create" className="ml-auto">
+          <button className="btn-primary">New Ticket</button>
+        </Link>
       </nav>
       <Suspense fallback={<Loading/>}>
         <TicketsList />
